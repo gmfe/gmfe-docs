689e3c7e4976d74bece16a27feba033401e885c9
actions <actions@github.com>
2020-03-16 04:16:28 +0000

    build docs

--- .storybook/i18n.js
+++
@@ -1,6 +0,0 @@
-import { setLocale } from '../packages/locales/src'
-
-let lng = localStorage.getItem('_react-gm_lng')
-lng = JSON.parse(lng)
-console.log('lng', lng)
-setLocale(lng)
--- packages/frame/src/index.js
+++
@@ -1,9 +0,0 @@
-import Framework from './framework'
-import CopyRight from './copyright'
-import Info from './info'
-import Breadcrumb from './breadcrumb'
-import Left from './left'
-import RightTop from './right_top'
-import FullTab from './full_tab'
-
-export { Framework, Left, RightTop, CopyRight, Info, Breadcrumb, FullTab }
